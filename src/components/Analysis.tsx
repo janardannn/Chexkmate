c0c0ba9cf07865b21170d2a575b2fe6412ccd37f
janardannn <janardonhazarika7@gmail.com>
2025-03-16 03:52:49 +0530

    wasted 2 hrs, rm -rfed 2 times and git rebased 2 times and finally learned that tailwind has changed to v4 and tailwind.config doesn't work anymore

--- src/components/Analysis.tsx
+++ src/components/Analysis.tsx
@@ -3,7 +3,7 @@
 export default function Analysis() {
     return (
         <div className="border-2 border-black w-[380px] h-[651px] p-2 rounded-sm">
-            Game Analysis
+            <p className="text-3xl font-paytone">Game Analysis</p>
         </div>
     );
 }
--- src/components/GamePGN.tsx
+++ src/components/GamePGN.tsx
@@ -1,5 +1,5 @@
 export default function () {
     return (
-        <textarea className="border-2 border-black w-[380px] h-[60px] p-2 rounded-sm" placeholder={"Enter Game PGN here"} />
+        <textarea className="border-2 border-black w-[380px] h-[60px] p-2 rounded-sm font-outfit" placeholder={"Enter Game PGN here"} />
     );
 }
